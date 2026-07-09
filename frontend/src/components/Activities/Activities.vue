6d5e56ec01f8df8a26cc1d800658d3bcae67090e
sam-mishra3005 <phoenixkumar2002@gmail.com>
2026-07-09 13:09:03 +0530

    fix: restore missing Activities.vue to unbreak build

---
+++ frontend/src/components/Activities/Activities.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="activities-placeholder border rounded-md">
+    <p class="text-gray-500 p-4">Activity timeline will appear here.</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const emailBox = ref({ show: false })
+const all_activities = ref({ reload: () => {} })
+
+function changeTabTo(tab) {
+  console.log('Tab changed to', tab)
+}
+
+defineExpose({
+  emailBox,
+  all_activities,
+  changeTabTo
+})
+</script>
